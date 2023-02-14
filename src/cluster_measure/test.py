14ecda9027af365eb92291255fcb96dec187ac2a
Wings <wingswad@gmail.com>
2023-02-14 12:30:10 +0000

    refactor: 💡 clean project

--- src/cluster_measure/test.py
+++ src/cluster_measure/test.py
@@ -1,3 +1,3 @@
 from ..distance_compute import config
-config.setConfig(config.speciesType.human, config.chainType.alpha)
-print(config.getColumns())
+config.set_config(config.speciesType.human, config.chainType.alpha)
+print(config.get_columns())
