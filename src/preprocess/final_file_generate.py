1fc6fff895ef50f5afed8c0ccbf96179afbed240
Qian52671 <Qianyiliu52671@gmail.com>
2023-03-05 13:06:58 +0000

    gliph tcr file2

--- src/preprocess/final_file_generate.py
+++ src/preprocess/final_file_generate.py
@@ -1,13 +1,14 @@
 import pandas as pd
 
 
-def generate_final_file(file_path='./file.tsv', save_path='./final_file.tsv'):
+def generate_final_file(file_path='./file.tsv', save_path='./final_file.txt'):
     df = pd.read_table(file_path).loc[:, ['CDR3b', 'TRBV', 'TRBJ', 'CDR3a', 'TRAV', 'TRAJ', 'meta.subject.id']]
     df['TRBV'] = df['TRBV'].str.replace(r'\*01', '', regex=True)
     df['TRBJ'] = df['TRBJ'].str.replace(r'\*01', '', regex=True)
     df['TRAV'] = df['TRAV'].str.replace(r'\*01', '', regex=True)
     df['TRAJ'] = df['TRAJ'].str.replace(r'\*01', '', regex=True)
     df = df.rename(columns={'meta.subject.id': 'Patient'})
+    df['Patient'] = df['Patient'].str.replace(' ', '')
     df_count = df.groupby(df.columns.tolist()).size().reset_index().rename(columns={0: 'Counts'})
     df = df.drop_duplicates().merge(df_count, on=df.columns.tolist(), how='left')
     df['Counts'] = df['Counts'].fillna(1)
