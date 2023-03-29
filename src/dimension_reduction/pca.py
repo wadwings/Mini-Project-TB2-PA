315b86962295807eacf11cb031f87721a03e77df
Qian52671 <Qianyiliu52671@gmail.com>
2023-03-29 12:16:57 +0100

    sevral dimension reduction method

--- src/dimension_reduction/pca.py
+++ src/dimension_reduction/pca.py
@@ -1,23 +1,10 @@
 from sklearn.decomposition import PCA,KernelPCA,SparsePCA
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import StandardScaler
 from sklearn.manifold import TSNE
-import numpy as np
-from sklearn.preprocessing import QuantileTransformer
 import umap
-from sklearn.model_selection import train_test_split
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.neighbors import KNeighborsClassifier, NeighborhoodComponentsAnalysis
-from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
-
-
-
-
-
-import pandas as pd
-import numpy as np
-
+import plotly.express as px
+from sklearn.manifold import MDS
 
 # def pca_decomposition(data):
 #     clf = PCA(n_components=2)
@@ -35,80 +22,9 @@ def switch_standard_data(feature):
 
 
 def pca_plot(feature,target):
-    n_neighbors = 40
-    random_state = 42
-    X = feature
-    y = target
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.4, stratify=y, random_state=random_state
-    )
-
-    # Reduce dimension to 2 with PCA
-    pca = make_pipeline(StandardScaler(), PCA(n_components=2, random_state=random_state))
-
-    # Reduce dimension to 2 with LinearDiscriminantAnalysis
-    lda = make_pipeline(StandardScaler(), LinearDiscriminantAnalysis(n_components=2))
-
-    # Reduce dimension to 2 with NeighborhoodComponentAnalysis
-    nca = make_pipeline(
-        StandardScaler(),
-        NeighborhoodComponentsAnalysis(n_components=3, random_state=random_state),
-    )
-
-    # Use a nearest neighbor classifier to evaluate the methods
-    knn = KNeighborsClassifier(n_neighbors=n_neighbors)
-
-    # Make a list of the methods to be compared
-    dim_reduction_methods = [("PCA", pca), ("LDA", lda), ("NCA", nca)]
-
-    # plt.figure()
-    for i, (name, model) in enumerate(dim_reduction_methods):
-        plt.figure()
-        # plt.subplot(1, 3, i + 1, aspect=1)
-
-        # Fit the method's model
-        model.fit(X_train, y_train)
-
-        # Fit a nearest neighbor classifier on the embedded training set
-        knn.fit(model.transform(X_train), y_train)
-
-        # Compute the nearest neighbor accuracy on the embedded test set
-        acc_knn = knn.score(model.transform(X_test), y_test)
-
-        # Embed the data set in 2 dimensions using the fitted model
-        X_embedded = model.transform(X)
-
-        # Plot the projected points and show the evaluation score
-        plt.scatter(X_embedded[:, 0], X_embedded[:, 1], c=y, s=30, cmap="Set1")
-        plt.title(
-            "{}, KNN (k={})\nTest accuracy = {:.2f}".format(name, n_neighbors, acc_knn)
-        )
-    plt.show()
-
-    nca.fit(X_train, y_train)
-    knn.fit(nca.transform(X_train), y_train)
-    acc_knn = knn.score(model.transform(X_test), y_test)
-    X_embedded = nca.transform(X)
-    plt.title(
-        "TEST UMAP{}, KNN (k={})\nTest accuracy = {:.2f}".format(name, n_neighbors, acc_knn)
-    )
-    plt.scatter(X_embedded[:, 0], X_embedded[:, 1], c=y, s=30, cmap="Set1")
-    plt.show()
-
-
-    m = "euclidean"
-
-    umap_model = umap.UMAP(random_state=42, n_neighbors=20, min_dist=0.6, n_components=2, metric=m)
-    embedding = umap_model.fit_transform(X_embedded)
-    plt.scatter(embedding[:, 0], embedding[:, 1], c=target, cmap='Spectral', s=5)
-    plt.title('UMAP')
-    plt.show()
-
-'''
-def pca_plot(feature,target):
     feature = switch_standard_data(feature)
 
-    pca = PCA(n_components=2, svd_solver="randomized", n_oversamples= 50,power_iteration_normalizer='LU',random_state=42)
+    pca = PCA(n_components=2,random_state=42)
     X_pca = pca.fit_transform(feature)
     label = target.values
     # plt.figure(4)
@@ -120,7 +36,38 @@ def pca_plot(feature,target):
 
     plt.title('PCA')
     plt.show()
-'''
+def pca_plot1(feature,target):
+    feature = switch_standard_data(feature)
+
+    pca = PCA(n_components=2,random_state=42)
+    X_pca = pca.fit_transform(feature)
+    label = target
+    # plt.figure(4)
+
+    plt.scatter(X_pca[:, 0], X_pca[:, 1], c=label, cmap='viridis')
+    plt.xlabel('PCA Component 1')
+    plt.ylabel('PCA Component 2')
+    plt.colorbar()
+
+    plt.title('Force Assignment PCA (Target: Antigen Species)')
+    plt.show()
+
+def kernel_pca(feature,target):
+    kernel_pca = KernelPCA(n_components=7, kernel='linear')
+    X_kernel_pca = kernel_pca.fit_transform(feature)
+
+    label = target
+    # plt.figure(4)
+
+    plt.scatter(X_kernel_pca[:, 0], X_kernel_pca[:, 1], c=label, cmap='viridis')
+    plt.xlabel('PCA Component 1')
+    plt.ylabel('PCA Component 2')
+    plt.colorbar()
+
+    plt.title('Kernel PCA')
+    plt.show()
+
+
 
 '''
 def SparsePCA_plot(feature,target):
@@ -152,12 +99,21 @@ def tsne_plot(feature, target):
 
     colors = target
     RS = 42
-    tsne = TSNE(n_components=2, perplexity=3)
+    tsne = TSNE(n_components=2, metric='precomputed')
     X_tsne = tsne.fit_transform(feature)
 
     fig, ax = fashion_scatter(X_tsne, colors)
+    plt.title('Tsne')
     plt.show()
-'''
+
+
+    '''
+    fig = px.scatter(
+        X_tsne,
+        color=colors, labels={'color': 'mhc class'}
+    )
+    fig.show()
+
     def fashion_scatter(x, colors):
         fig, ax = plt.subplots(figsize=(6, 6))
         ax.scatter(x[:, 0], x[:, 1], c=colors, cmap='rainbow')
@@ -175,17 +131,27 @@ def tsne_plot(feature, target):
 
     
 
-'''
+
 def umap_plot(feature, target):
 
     m = "euclidean"
 
-    umap_model = umap.UMAP(random_state=42, n_neighbors=100, min_dist=0.1, n_components=2, metric=m)
+    umap_model = umap.UMAP(random_state=42, n_neighbors=10, min_dist=0.2, n_components=2, metric=m)
     embedding = umap_model.fit_transform(feature)
 
     plt.scatter(embedding[:, 0], embedding[:, 1], c=target, cmap='Spectral', s=5)
-    plt.title('UMAP')
+    plt.colorbar()
+    plt.title('Force Assignment UMAP (Target: Antigen Species)')
     plt.show()
 
-'''
+
+def mds_plot(feature,target):
+    mds = MDS(n_components=2,random_state=42)
+    X = mds.fit_transform(feature)
+    label = target
+
+    plt.scatter(X[:, 0], X[:, 1], c=label, cmap='viridis')
+    plt.title('MDS')
+    plt.show()
+
 
