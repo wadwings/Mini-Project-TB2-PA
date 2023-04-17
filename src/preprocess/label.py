92d51e5f2147c674ccb559fad2f7f6cd17db9477
Qian52671 <Qianyiliu52671@gmail.com>
2023-04-17 23:13:34 +0100

    label update

--- src/preprocess/label.py
+++ src/preprocess/label.py
@@ -3,6 +3,7 @@ import copy
 import pandas as pd
 
 from src.preprocess.setup import config
+from src.preprocess.feature_filter import do_preprocess
 # from src.preprocess.feature_filter import load_data
 
 
@@ -17,7 +18,6 @@ def generate_label(data: pd.DataFrame, label: config.labelType = None):
     index_map = {}
     index = 0
     labelstr = lambda x: '.'.join(x.to_frame().T[columns].to_numpy()[0])
-
     # print(labelstr(data.iloc[0, :]))
     for _, row in data.iterrows():
         # print(row)
@@ -28,21 +28,25 @@ def generate_label(data: pd.DataFrame, label: config.labelType = None):
             index += 1
 
     data['label'] = [index_map[labelstr(r)] for i, r in data.iterrows()]
-    top3_labels = data['label'].value_counts().head(3).index.tolist()
-    print(f'top3:',top3_labels)
-    data = data[data['label'].isin(top3_labels)]
-
-    # reverse_map = {}
-    # for key in index_map:
-    #     reverse_map[index_map[key]] = key
     reverse_map = {}
     for key in index_map:
-        if index_map[key] in top3_labels:
-            reverse_map[index_map[key]] = key
-
+        reverse_map[index_map[key]] = key
     return data, reverse_map
 
     # return pd.DataFrame(filtered_df)
+
+def select_top_label(data):
+    top_labels = data['label'].value_counts().head(3).index.tolist()
+    print(f'top:',top_labels)
+    data1 = data[data['label'].isin(top_labels)]
+    print(data1)
+
+    # reverse_map = {}
+    # for key in index_map:
+    #     if index_map[key] in top3_labels:
+    #         reverse_map[index_map[key]] = key
+
+    return data1,top_labels
 
 
 
